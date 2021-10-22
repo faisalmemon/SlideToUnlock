59e03ba6862f3f383141661846d0935ef6a4b14e
Faisal Memon <faisalmemonuk@yahoo.com>
2021-10-22 14:40:05 +0100

    Initial Commit

---
+++ SlideToUnlock/SlideToUnlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideToUnlockApp.swift
+//  SlideToUnlock
+//
+//  Created by Faisal Memon on 22/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SlideToUnlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
